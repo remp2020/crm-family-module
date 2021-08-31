0a4f67c2c63f4444324f044ed96f17d5a871ab7b
Dominik Skerhak <dominik.skerhak@gmail.com>
2021-08-31 09:54:41 +0200

    Nette v3.0 - Use CompilerExtension->loadDefinitionsFromConfig()
    `Nette\DI\Compiler::loadDefinitions()` was deprecated. Switching to
    `Nette\DI\CompilerExtension->loadDefinitionsFromConfig()` in all CRM
    extensions.
    
    remp/crm#1979

--- src/DI/FamilyModuleExtension.php
+++ src/DI/FamilyModuleExtension.php
@@ -3,7 +3,6 @@
 namespace Crm\FamilyModule\DI;
 
 use Kdyby\Translation\DI\ITranslationProvider;
-use Nette\DI\Compiler;
 use Nette\DI\CompilerExtension;
 
 final class FamilyModuleExtension extends CompilerExtension implements ITranslationProvider
@@ -11,8 +10,7 @@ final class FamilyModuleExtension extends CompilerExtension implements ITranslat
     public function loadConfiguration()
     {
         // load services from config and register them to Nette\DI Container
-        Compiler::loadDefinitions(
-            $this->getContainerBuilder(),
+        $this->compiler->loadDefinitionsFromConfig(
             $this->loadFromFile(__DIR__.'/../config/config.neon')['services']
         );
     }
