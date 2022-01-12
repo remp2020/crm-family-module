1fd18866f493cecdec2eb9fd1a78b2d664a92f4a
Dominik Skerhak <dominik.skerhak@gmail.com>
2022-01-12 15:42:01 +0100

    Nette API: Load authorization from handler instead from argument
    remp/crm#333

--- src/Api/ListFamilyRequestsApiHandler.php
+++ src/Api/ListFamilyRequestsApiHandler.php
@@ -26,6 +26,7 @@ class ListFamilyRequestsApiHandler extends ApiHandler
 
     public function handle(array $params): ApiResponseInterface
     {
+        $authorization = $this->getAuthorization();
         $data = $authorization->getAuthorizedData();
         if (!isset($data['token'])) {
             $response = new JsonResponse([
