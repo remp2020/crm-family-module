45624d0c353500c141e9952c125ad98ae594410e
Dominik Skerhak <dominik.skerhak@gmail.com>
2023-06-13 13:33:03 +0200

    Fix rollback for few migrations
    Few IrreversibleMigrationException exceptions throws:
    
    - Column type "mediumtext" is not supported by MySQL
    - Cannot reverse a "Phinx\Db\Action\ChangeColumn" command
    - Cannot reverse a "Phinx\Db\Action\RemoveColumn" command
    - Cannot reverse a "Phinx\Db\Action\DropIndex" command
    
    Few MySQL errors:
    
    - Cannot drop index '...': needed in a foreign key constraint
    - Can't DROP 'type'; check that column/key exists
    
    Older migrations were fixed just to prevent copy -> paste errors in
    future.

--- src/migrations/20220530121649_nullable_family_subscription_types_slave_subscription_type_id.php
+++ src/migrations/20220530121649_nullable_family_subscription_types_slave_subscription_type_id.php
@@ -4,10 +4,17 @@ use Phinx\Migration\AbstractMigration;
 
 class NullableFamilySubscriptionTypesSlaveSubscriptionTypeId extends AbstractMigration
 {
-    public function change()
+    public function up()
     {
         $this->table('family_subscription_types')
             ->changeColumn('slave_subscription_type_id', 'integer', ['null' => true])
+            ->update();
+    }
+
+    public function down()
+    {
+        $this->table('family_subscription_types')
+            ->changeColumn('slave_subscription_type_id', 'integer', ['null' => false])
             ->update();
     }
 }
