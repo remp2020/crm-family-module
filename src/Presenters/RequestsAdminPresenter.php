d6fa45431ace5a65f3b96cc94909ea2903a153d5
Miroslav Svitok <mirosvitok@gmail.com>
2024-03-25 15:44:43 +0100

    Fix nette/application 3.2 incompatibility - $user Latte variable type mismatch
    Due to change introduced in https://github.com/nette/application/commit/244fc48af976a5c85912b153d1e6c300cd694a40
    type-related exception occured in all templates using $user variable.
    
    This fix renames $user variable to $dbUser in all cases when there is a type mismatch.

--- src/Presenters/RequestsAdminPresenter.php
+++ src/Presenters/RequestsAdminPresenter.php
@@ -40,7 +40,7 @@ class RequestsAdminPresenter extends AdminPresenter
 
         $customFamilySubscriptionTypes = $this->familySubscriptionTypesRepository->getCustomizableSubscriptionTypes();
 
-        $this->template->user = $user;
+        $this->template->dbUser = $user;
         $this->template->subscriptionTypes = $customFamilySubscriptionTypes;
     }
 
