1c749ff6eaea0a53ef7e2ecefd226ae7b3cce65c
Miroslav Svitok <mirosvitok@gmail.com>
2024-11-18 14:26:06 +0000

    Fix recurrent payments VAT resolving for family master subscriptions
    Previously, VAT changes were ignored, now current VAT of subscription type items is always used.
    
    remp/helpdesk#3159

--- src/DataProviders/RecurrentPaymentPaymentItemContainerDataProvider.php
+++ src/DataProviders/RecurrentPaymentPaymentItemContainerDataProvider.php
@@ -7,6 +7,7 @@ use Crm\PaymentsModule\DataProviders\RecurrentPaymentPaymentItemContainerDataPro
 use Crm\PaymentsModule\Models\PaymentItem\PaymentItemContainer;
 use Crm\PaymentsModule\Models\PaymentItem\PaymentItemContainerFactory;
 use Crm\SubscriptionsModule\Models\PaymentItem\SubscriptionTypePaymentItem;
+use Crm\SubscriptionsModule\Repositories\SubscriptionTypeItemsRepository;
 use Nette\Database\Table\ActiveRow;
 
 final class RecurrentPaymentPaymentItemContainerDataProvider implements RecurrentPaymentPaymentItemContainerDataProviderInterface
@@ -15,6 +16,7 @@ final class RecurrentPaymentPaymentItemContainerDataProvider implements Recurren
     public function __construct(
         private FamilySubscriptionTypesRepository $familySubscriptionTypesRepository,
         private PaymentItemContainerFactory $paymentItemContainerFactory,
+        private SubscriptionTypeItemsRepository $subscriptionTypeItemsRepository,
     ) {
     }
 
@@ -22,14 +24,33 @@ final class RecurrentPaymentPaymentItemContainerDataProvider implements Recurren
         ActiveRow $recurrentPayment,
         ActiveRow $subscriptionType,
     ): ?PaymentItemContainer {
-        // for master family subscription, create payment container just by copying previous payment items
-        // (of type SubscriptionTypePaymentItem) and ignore all potential price differences
-        if ($this->familySubscriptionTypesRepository->isMasterSubscriptionType($subscriptionType)) {
-            return $this->paymentItemContainerFactory->createFromPayment(
-                $recurrentPayment->parent_payment,
-                [SubscriptionTypePaymentItem::TYPE]
-            );
+        if (!$this->familySubscriptionTypesRepository->isMasterSubscriptionType($subscriptionType)) {
+            return null;
         }
-        return null;
+
+        // for master family subscription, create payment container just by copying previous payment items
+        // (of type SubscriptionTypePaymentItem) and ignore all potential price differences (but do not ignore VAT changes)
+        $paymentItemContainer = $this->paymentItemContainerFactory->createFromPayment(
+            $recurrentPayment->parent_payment,
+            [SubscriptionTypePaymentItem::TYPE]
+        );
+
+        foreach ($paymentItemContainer->items() as $item) {
+            // always force current VAT
+            if ($item instanceof SubscriptionTypePaymentItem) {
+                if (!$item->getSubscriptionTypeItemId()) {
+                    // In case there is no reference to original subscription type item,
+                    // we cannot check VAT, therefore skip the check.
+                    continue;
+                }
+
+                $subscriptionTypeItem = $this->subscriptionTypeItemsRepository->find($item->getSubscriptionTypeItemId());
+                if (!$subscriptionTypeItem) {
+                    throw new \RuntimeException("Missing subscription type item [{$item->getSubscriptionTypeItemId()}]");
+                }
+                $item->forceVat($subscriptionTypeItem->vat);
+            }
+        }
+        return $paymentItemContainer;
     }
 }
--- src/Tests/RecurrentPaymentPaymentItemContainerDataProviderTest.php
+++ src/Tests/RecurrentPaymentPaymentItemContainerDataProviderTest.php
@@ -3,43 +3,80 @@
 namespace Crm\FamilyModule\Tests;
 
 use Crm\ApplicationModule\Models\DataProvider\DataProviderManager;
-use Crm\FamilyModule\FamilyModule;
+use Crm\ApplicationModule\Seeders\CountriesSeeder;
+use Crm\FamilyModule\DataProviders\RecurrentPaymentPaymentItemContainerDataProvider;
+use Crm\PaymentsModule\DataProviders\RecurrentPaymentPaymentItemContainerDataProviderInterface;
 use Crm\PaymentsModule\Models\GatewayFactory;
 use Crm\PaymentsModule\Models\PaymentItem\PaymentItemContainer;
 use Crm\PaymentsModule\Models\PaymentProcessor;
 use Crm\PaymentsModule\Models\RecurrentPaymentsResolver;
+use Crm\PaymentsModule\Models\RecurrentPaymentsResolver\PaymentData;
 use Crm\PaymentsModule\Repositories\PaymentGatewaysRepository;
 use Crm\PaymentsModule\Repositories\PaymentsRepository;
 use Crm\PaymentsModule\Repositories\RecurrentPaymentsRepository;
+use Crm\PaymentsModule\Repositories\VatRatesRepository;
 use Crm\PaymentsModule\Seeders\TestPaymentGatewaysSeeder;
 use Crm\PaymentsModule\Tests\Gateways\TestRecurrentGateway;
 use Crm\SubscriptionsModule\Models\PaymentItem\SubscriptionTypePaymentItem;
+use Crm\SubscriptionsModule\Repositories\SubscriptionTypeItemsRepository;
 use Crm\UsersModule\Models\Auth\UserManager;
+use Crm\UsersModule\Repositories\CountriesRepository;
 use Nette\Database\Table\ActiveRow;
 
 final class RecurrentPaymentPaymentItemContainerDataProviderTest extends BaseTestCase
 {
-
-    /** @var PaymentsRepository */
-    private $paymentsRepository;
+    private PaymentsRepository $paymentsRepository;
+    private SubscriptionTypeItemsRepository $subscriptionTypeItemsRepository;
+    private CountriesRepository $countriesRepository;
+    private VatRatesRepository $vatRatesRepository;
+    private RecurrentPaymentsRepository $recurrentPaymentsRepository;
+    private RecurrentPaymentsResolver $recurrentPaymentsResolver;
 
     protected function setUp(): void
     {
         parent::setUp();
 
+        $this->subscriptionTypeItemsRepository = $this->getRepository(SubscriptionTypeItemsRepository::class);
         $this->paymentsRepository = $this->getRepository(PaymentsRepository::class);
+        $this->countriesRepository = $this->getRepository(CountriesRepository::class);
+        $this->recurrentPaymentsRepository = $this->getRepository(RecurrentPaymentsRepository::class);
+        $this->vatRatesRepository = $this->getRepository(VatRatesRepository::class);
+
+        $this->recurrentPaymentsResolver = $this->inject(RecurrentPaymentsResolver::class);
 
         $gatewayFactory = $this->inject(GatewayFactory::class);
-        $gatewayFactory->registerGateway(TestRecurrentGateway::GATEWAY_CODE, TestRecurrentGateway::class);
+        if (!in_array(TestRecurrentGateway::GATEWAY_CODE, $gatewayFactory->getRegisteredCodes(), true)) {
+            $gatewayFactory->registerGateway(TestRecurrentGateway::GATEWAY_CODE, TestRecurrentGateway::class);
+        }
 
-        $familyModule = $this->container->createInstance(FamilyModule::class);
-        $familyModule->registerDataProviders($this->inject(DataProviderManager::class));
+        $dataProviderManager = $this->inject(DataProviderManager::class);
+        $dataProviderManager->registerDataProvider(
+            RecurrentPaymentPaymentItemContainerDataProviderInterface::PATH,
+            $this->inject(RecurrentPaymentPaymentItemContainerDataProvider::class)
+        );
+
+        // Setting countries and VATs
+        // in all tests, we use France as foreign country
+        $this->countriesRepository->setDefaultCountry('SK');
+        $this->vatRatesRepository->upsert($this->countriesRepository->defaultCountry(), 20, 10, 10);
     }
+
+    protected function requiredRepositories(): array
+    {
+        return [
+            ...parent::requiredRepositories(),
+            SubscriptionTypeItemsRepository::class,
+            CountriesRepository::class,
+            VatRatesRepository::class,
+        ];
+    }
+
 
     protected function requiredSeeders(): array
     {
         return [
             ...parent::requiredSeeders(),
+            CountriesSeeder::class,
             TestPaymentGatewaysSeeder::class,
         ];
     }
@@ -47,49 +84,52 @@ final class RecurrentPaymentPaymentItemContainerDataProviderTest extends BaseTes
     public function testRecurrentPaymentRenewalOfCustomizableMasterSubscription()
     {
         [$masterSubscriptionType, $printSlaveSubscriptionType, $webSlaveSubscriptionType] = $this->seedFamilyCustomizableSubscriptionType();
+        $masterWebSubscriptionTypeItem = $this->subscriptionTypeItemsRepository->subscriptionTypeItems($masterSubscriptionType)
+            ->where('name', 'Web')
+            ->fetch();
+        $masterPrintSubscriptionTypeItem = $this->subscriptionTypeItemsRepository->subscriptionTypeItems($masterSubscriptionType)
+            ->where('name', 'Print')
+            ->fetch();
 
         $user = $this->inject(UserManager::class)->addNewUser('user@example.com', false, 'unknown', null, false);
         $paymentGateway = $this->getRepository(PaymentGatewaysRepository::class)
             ->findByCode(TestRecurrentGateway::GATEWAY_CODE);
 
-        // Payment item of customizable master subscription can have different payment items than associated with
-        // subscription type
+        // Setting custom price and amount (VAT is 20% in seeded subscription types)
+        SubscriptionTypePaymentItem::fromSubscriptionType($masterSubscriptionType);
         $paymentItemContainer = new PaymentItemContainer();
-        $paymentItemContainer->addItem(new SubscriptionTypePaymentItem(
-            $webSlaveSubscriptionType->id,
-            $webSlaveSubscriptionType->name,
-            10,
-            20,
-            2
-        ));
-        $paymentItemContainer->addItem(new SubscriptionTypePaymentItem(
-            $printSlaveSubscriptionType->id,
-            $printSlaveSubscriptionType->name,
-            5,
-            20,
-            3
-        ));
-
-        $payment = $this->makePayment($user, $masterSubscriptionType, $paymentGateway, $paymentItemContainer);
-
-        /** @var RecurrentPaymentsRepository $recurrentPaymentsRepository */
-        $recurrentPaymentsRepository = $this->getRepository(RecurrentPaymentsRepository::class);
-        $recurrentPayment = $recurrentPaymentsRepository->recurrent($payment);
-
-        /** @var RecurrentPaymentsResolver $recurrentPaymentsResolver */
-        $recurrentPaymentsResolver = $this->inject(RecurrentPaymentsResolver::class);
-        $paymentData = $recurrentPaymentsResolver->resolvePaymentData($recurrentPayment);
+        $paymentItemContainer->addItem(
+            SubscriptionTypePaymentItem::fromSubscriptionTypeItem($masterWebSubscriptionTypeItem)
+            ->forceCount(2)
+            ->forcePrice(10)
+        );
+        $paymentItemContainer->addItem(
+            SubscriptionTypePaymentItem::fromSubscriptionTypeItem($masterPrintSubscriptionTypeItem)
+                ->forceCount(3)
+                ->forcePrice(5)
+        );
 
         // Assert payment items are correctly copied from parent payment,
-        // differences in price, amount and VAT should be ignored
-        $items = $paymentData->paymentItemContainer->items();
-        $this->assertEquals(10, $items[0]->unitPrice());
-        $this->assertEquals(20, $items[0]->vat());
-        $this->assertEquals(2, $items[0]->count());
+        // differences in prices should be ignored
+        // VAT changes should NOT be ignored
 
-        $this->assertEquals(5, $items[1]->unitPrice());
-        $this->assertEquals(20, $items[1]->vat());
-        $this->assertEquals(3, $items[1]->count());
+        $payment1 = $this->makePayment($user, $masterSubscriptionType, $paymentGateway, $paymentItemContainer);
+        $payment2 = $this->makeRecurrentPayment($payment1, $paymentGateway);
+        $this->assertPaymentItemsEqualTo($payment2, [
+            'web' => ['name' => 'Web', 'amount' => 10, 'vat' => 20, 'count' => 2],
+            'print' => ['name' => 'Print', 'amount' => 5, 'vat' => 20, 'count' => 3],
+        ]);
+
+        // Change VAT for Print
+        $this->subscriptionTypeItemsRepository->update($masterPrintSubscriptionTypeItem, [
+            'vat' => 12,
+        ], true);
+
+        $payment3 = $this->makeRecurrentPayment($payment2, $paymentGateway);
+        $this->assertPaymentItemsEqualTo($payment3, [
+            'web' => ['name' => 'Web', 'amount' => 10, 'vat' => 20, 'count' => 2],
+            'print' => ['name' => 'Print', 'amount' => 5, 'vat' => 12, 'count' => 3],
+        ]);
     }
 
     private function makePayment(
@@ -108,5 +148,50 @@ final class RecurrentPaymentPaymentItemContainerDataProviderTest extends BaseTes
         // Make manual payment
         $this->inject(PaymentProcessor::class)->complete($payment, fn() => null);
         return $this->paymentsRepository->find($payment->id);
+    }
+
+    private function makeRecurrentPayment(ActiveRow $parentPayment, ActiveRow $paymentGateway): ActiveRow
+    {
+        $recurrent = $this->recurrentPaymentsRepository->recurrent($parentPayment);
+
+        /** @var PaymentData $paymentData */
+        $paymentData = $this->recurrentPaymentsResolver->resolvePaymentData($recurrent);
+
+        $payment = $this->paymentsRepository->add(
+            subscriptionType: $paymentData->subscriptionType,
+            paymentGateway: $paymentGateway,
+            user: $recurrent->user,
+            paymentItemContainer: $paymentData->paymentItemContainer,
+            recurrentCharge: true,
+            paymentCountry: $paymentData->paymentCountry,
+        );
+
+        $this->recurrentPaymentsRepository->update($recurrent, [
+            'payment_id' => $payment->id,
+        ]);
+
+        $this->inject(PaymentProcessor::class)->complete($payment, fn() => null);
+        $payment = $this->paymentsRepository->find($payment->id);
+        $this->recurrentPaymentsRepository->setCharged($recurrent, $payment, 'OK', 'OK');
+        return $payment;
+    }
+
+    private function assertPaymentItemsEqualTo(ActiveRow $payment, array $itemsToCompare): void
+    {
+        $paymentItemsData = [];
+        foreach ($payment->related('payment_items') as $pi) {
+            $paymentItemsData[$pi->name] = ['name' => $pi->name, 'amount' => $pi->amount, 'vat' => $pi->vat, 'count' => $pi->count];
+        }
+        sort($itemsToCompare);
+        sort($paymentItemsData);
+        foreach (array_keys($itemsToCompare) as $k) {
+            $this->assertEqualsCanonicalizing($itemsToCompare[$k], $paymentItemsData[$k]);
+        }
+        // check sum of payment
+        $sumItemsToCompare = 0;
+        foreach ($itemsToCompare as $item) {
+            $sumItemsToCompare += $item['amount'] * $item['count'];
+        }
+        $this->assertEquals($payment->amount, $sumItemsToCompare);
     }
 }
