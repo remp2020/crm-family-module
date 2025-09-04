531b85d1eac998bb7ac02441aa0d1b52488d1845
Adam Žoldák <zoldakadam@gmail.com>
2025-09-04 11:19:24 +0000

    Fixed `IsFamilySlaveCriteria` scenario criteria to check if its family slave through `family_requests` table, instead of `family_subscription_types`
    remp/respekt#454

--- src/Models/Scenarios/IsFamilySlaveCriteria.php
+++ src/Models/Scenarios/IsFamilySlaveCriteria.php
@@ -21,9 +21,9 @@ class IsFamilySlaveCriteria implements ScenariosCriteriaInterface
         $values = $paramValues['is_family_slave'];
 
         if ($values->selection) {
-            $selection->where('subscription_type:family_subscription_types(slave_subscription_type_id).id IS NOT NULL');
+            $selection->where(':family_requests(slave_subscription_id).id IS NOT NULL');
         } else {
-            $selection->where('subscription_type:family_subscription_types(slave_subscription_type_id).id IS NULL');
+            $selection->where(':family_requests(slave_subscription_id).id IS NULL');
         }
 
         return true;
